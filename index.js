32bd13b2846f29ae7d8a1ba238d795ba5269bf84
Anirban Paul <anirban.paul@gmail.com>
2022-09-09 23:18:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+var exphbs = require('express-handlebars');
+
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars'); 
+
+const newstuff = "This is completely different";
+
+app.get('/', function (req, res) {
+res.render('home', { stuff:  newstuff} );
+}); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen (PORT, () => console.log('Server listening on port ' + PORT)); 
